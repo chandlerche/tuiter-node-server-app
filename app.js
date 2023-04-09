a2e9b7268ef3b2af9558ca5f093119f66ce637f3
“chandlerche” <54366747+chandlerche@users.noreply.github.com>
2023-04-09 06:41:54 -0400

    a9

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import mongoose from "mongoose";
 import cors from 'cors'
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+mongoose.connect('mongodb://127.0.0.1:27017/tuiter);
 const app = express()
 app.use(cors())
 app.use(express.json());
